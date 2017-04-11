8850d61cd73475fbab3b2dcdc4882ba10f245ef8
Giannis Gasteratos <igasteratos@gmail.com>
2017-04-11 10:10:22 +0000

    test

--- src/migrations/2017_02_13_090952_geo.php
+++ src/migrations/2017_02_13_090952_geo.php
@@ -12,21 +12,24 @@ class Geo extends Migration
      * @return void
      */
     public function up() {
-        Schema::create('geo', function (Blueprint $table) {
-            $table->increments('id');
-            $table->integer('parent_id')->nullable();
-            $table->integer('left')->nullable();
-            $table->integer('right')->nullable();
-            $table->integer('depth')->default(0);
-            // $table->integer('geoid');
-            $table->char('name', 60);
-            $table->json('alternames');
-            $table->char('country', 2);
-            $table->char('level', 10);
-            $table->bigInteger('population');
-            $table->decimal('lat',9,6);
-            $table->decimal('long',9,6);
-        });
+        if (!Schema::hasTable('geo')) {
+            Schema::create('geo', function (Blueprint $table) {
+                $table->increments('id');
+                $table->integer('parent_id')->nullable();
+                $table->integer('left')->nullable();
+                $table->integer('right')->nullable();
+                $table->integer('depth')->default(0);
+                // $table->integer('geoid');
+                $table->char('name', 60);
+                $table->json('alternames');
+                $table->char('country', 2);
+                $table->char('level', 10);
+                $table->bigInteger('population');
+                $table->decimal('lat',9,6);
+                $table->decimal('long',9,6);
+            });
+        }
+
     }
 
     /**
