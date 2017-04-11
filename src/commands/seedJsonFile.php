1a3c828dc14d37e8a02b66cb55d49b61087d393b
Giannis Gasteratos <igasteratos@gmail.com>
2017-04-11 22:25:46 +0000

    test

--- src/commands/seedJsonFile.php
+++ src/commands/seedJsonFile.php
@@ -39,7 +39,6 @@ class seedJsonFile extends Command
 
         $filename = storage_path("geo/{$filename}.json");
         $this->info("Parsing file: $filename");
-        $append =  $this->option('append');
 
         $data = json_decode(file_get_contents($filename), true);
         if($data === null){
