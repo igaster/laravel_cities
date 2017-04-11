f5052bd04ab7652831f550b8a57f69d8594c7c38
Giannis Gasteratos <igasteratos@gmail.com>
2017-04-11 22:17:36 +0000

    test

--- src/commands/seedJsonFile.php
+++ src/commands/seedJsonFile.php
@@ -24,23 +24,23 @@ class seedJsonFile extends Command
     public function handle() {
         $start = microtime(true);
 
-        $fileName = $this->argument('file');
+        $filename = $this->argument('file');
 
         if(empty($filename)){
             $this->info("Available json files:");
             $this->info("---------------------");
             $files = array_diff(scandir(storage_path("geo")), ['.','..']);
             foreach ($files as $file)
-                $this->comment(' '.substr($file, 0, strpos($file, '.json')));
-
+                if(strpos($file, '.json')!==false)
+                    $this->comment(' '.substr($file, 0, strpos($file, '.json')));
             $this->info("---------------------");
             $filename   = $this->ask('Choose File to restore:');
         }
 
-        $fileName = storage_path("geo/{$fileName}.json");
+        $filename = storage_path("geo/{$filename}.json");
         $append =  $this->option('append');
 
-        $data = json_decode(file_get_contents($fileName), true);
+        $data = json_decode(file_get_contents($filename), true);
         if($data === null){
             $this->error("Error decoding json file. Check for syntax errors.");
             exit();
